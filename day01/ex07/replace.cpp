687f2088824cbd78805673706c4b9d6e01a9d814
Aurore LIANDIER <aliandie@e2r2p16.42.fr>
2019-05-20 16:16:53 +0200

    [DONE]DAY01EX07 [TODO]DAY01 EX08 EX09 EX10

--- day01/ex07/replace.cpp
+++ day01/ex07/replace.cpp
@@ -2,34 +2,54 @@
 #include <sstream>
 #include <string>
 #include <iostream>
+#include "stdlib.h"
+
+void    replace(char const *av[]){
+
+    std::ifstream   inputFile(av[1]);
+    std::ofstream   outputFile;
+
+    std::string     newFile(av[1]);
+    std::ostringstream streamBuff;
+
+    outputFile.open(newFile.append(".replace"), std::fstream::out);
+
+    streamBuff << inputFile.rdbuf();
+    std::string line = streamBuff.str();
+
+    while (line.find(std::string(av[2])) != std::string::npos)
+    {
+        line.replace(line.find(std::string(av[2])), std::string(av[2]).length(), std::string(av[3]));
+    }
+    outputFile << line;
+
+    inputFile.close();
+    outputFile.close();
+}
+
+bool  checkError(char const *av[]) {
+    std::ifstream fichier(std::string(av[1]).c_str());
+
+    if(fichier.fail()){
+        std::cerr << "File does not exist or can not be read." << std::endl;
+        return false;
+    }
+    else if (std::string(av[2]).length() <= 0  || std::string(av[3]).length() <= 0){
+        std::cerr << "Target string and source string can not be empty." << std::endl;
+        return false;
+    }
+    return true;
+}
 
 int main(int ac, char const *av[])
 {
-	if (ac != 3){
-		std::cout << "usage: [FILE] [STRDEST] [STRSOURCE]" << std::endl;
+
+	if (ac != 4){
+		std::cerr << "usage: [FILE] [STR_TARGET] [STR_SOURCE]" << std::endl;
 		return 0;
 	}
-	std::ifstream   infile(av[1]);
-	std::string     newFile(av[1]);
-
-
-	std::ofstream   file;
-
-	file.open(newFile.append(".replace"), std::fstream::out);
-    file << infile.rdbuf();
-	std::string line;
-    std::fo (std::getline(newFile, line)){
-        std::cout << line;
-    }
-
-	while (std::getline(infile, line))
-	{
-        std::size_t found = line.find(av[2]);
-        if (found != std::string::npos)
-            std::cout <<  ;
-        std::cout << found;
-	}
-
+	else if (checkError(av))
+	    replace(av);
 	return 0;
 }
 
