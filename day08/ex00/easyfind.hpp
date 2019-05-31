7bfa88e3aae952170d585f705e2d83b80653dc82
Aurore LIANDIER <aliandie@e2r13p11.42.fr>
2019-05-31 18:34:48 +0200

    [DONE]DAY08RX00

--- day08/ex00/easyfind.hpp
+++ day08/ex00/easyfind.hpp
@@ -1,8 +1,15 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
+#include <algorithm>
 
 template <typename T>
 int easyfind(T container, int occ){
-    for_each
+    typename T::iterator it;
+    typename T::iterator itBeg = container.begin();
+    typename T::iterator itEnd = container.end();
+
+        if ((it = find(itBeg, itEnd, occ)) != itEnd)
+            return std::distance(itBeg, it);
+        throw(std::runtime_error("Value not found."));
 }
 #endif
--- day08/ex00/main.cpp
+++ day08/ex00/main.cpp
@@ -1,5 +1,26 @@
 #include "easyfind.hpp"
-
+#include <iostream>
+#include <list>
 int main(){
+
+    std::list<int>	lst;
+
+    lst.push_back(1);
+    lst.push_back(2);
+    lst.push_back(4);
+    lst.push_back(3);
+    lst.push_back(5);
+
+    try{
+
+        std::cout <<"trying to find 5 in the lst : '1,2,4,3,5'...  return index : " << easyfind(lst, 5) << std::endl;
+        std::cout <<"trying to find 10 in the lst : '1,2,4,3,5'...  return index : " << easyfind(lst, 10) << std::endl;
+
+    }
+    catch (std::exception& e){
+        std::cout << e.what() << std::endl;
+    }
+
+
     return 0;
 }
